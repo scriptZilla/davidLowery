74292c4f5ede65a3d4893b5ae70c797cb1508038
scriptZilla <david.lowery@colorado.edu>
2014-05-15 16:12:25 -0600

    Add validation for Project model's fields

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ActiveRecord::Base
+  validates :title, length: { minimum: 4 }, 
+		    presence: true
+  validates :date, length: { minimum: 6 }, 
+		   presence: true
+  validates :web_address, length: { minimum: 8}, 
+		   presence: true
+  validates :description, length: { minimum: 4}, 
+		   presence: true
 end
